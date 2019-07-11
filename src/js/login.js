16e9d1cf9559afeed77bcfa0aa4f36eb1054f635
Nikhil Kumar <nikhil.kumar@escalesolutions.com>
2019-07-11 09:32:05 +0530

    forms api integration done

--- src/js/login.js
+++
@@ -1,9 +0,0 @@
-function onLogin() {
-    $("#loginErr").empty();
-    if($("#username").val()=="admin" && $("#password").val()=="admin") {
-        window.location.href = "/forms/about.html"
-        localStorage.setItem("loggedIn", true);
-    } else {
-        $("#loginErr").append("Wrong username/password");
-    }
-}
